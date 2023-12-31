ad1fee46baae724ffbd5b9efd795e77e4910eced
adhil12345 <84992437+adhil12345@users.noreply.github.com>
2023-12-31 22:44:18 +0530

    Update blackvenda-shoe-size.js

--- blackvenda-shoe-size.js
+++ blackvenda-shoe-size.js
@@ -23,3 +23,32 @@
   document.addEventListener("DOMContentLoaded", function () {
     handleSizes();
   });
+
+  // Wrap the code in a function to encapsulate variables and avoid polluting the global scope
+  function handleOutOfStock() {
+    var checksizOut = document.querySelectorAll(".contant-detaiels-here")[0].innerHTML.split("Size-[")[1].split("]")[0];
+
+    // Define an array of sizes
+    var sizes = ['35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', 'S', 'M_', 'L', 'X_', 'XX', '110CM', '115CM', '120CM', '130CM', '140CM', '150CM', '160CM'];
+
+    // Loop through sizes array
+    sizes.forEach(function (size) {
+      var sizeElement = document.querySelectorAll(`#siz${size}`)[0];
+      var stockElement = document.querySelectorAll(`#siz${size}1`)[0];
+
+      if (checksizOut.match(`-${size}`)) {
+        sizeElement.setAttribute("class", "size1");
+        stockElement.disabled = true;
+        stockElement.setAttribute("value", "Out of Stock");
+      }
+    });
+
+    // Display or hide the size container based on any digit in the checkSizOut string
+    var displaySize = document.querySelectorAll("#disPlaySize")[0];
+    checksizOut.match(/[0-9]/g) ? displaySize.style.display = "unset" : displaySize.style.display = "none";
+  }
+
+  // Call the function when the page is loaded
+  document.addEventListener("DOMContentLoaded", function () {
+    handleOutOfStock();
+  });
